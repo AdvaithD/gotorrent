dc27bb38759992db7ed653b207a6e330fee51519
Advaith Doosa <advaith@doosa.org>
2020-12-16 08:09:38 +0000

    bencode structs

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ import (
 )
 
 type bencodeInfo struct {
-  Pieces string
+  Pieces      string `bencode:"pieces"`
+  PieceLength int `bencode:"piece length"`
+  Length      int `bencode:"length"`
+  Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+  Announce string      `bencode:"announce"`
+  Info     bencodeInfo `bencode:"info"`
+}
+
+
+func Open(r io.Reader) (*bencodeTorrent, error) {
+  bto := bencodeTorrent{}
+  err := bencode.Unmarshal(r, &bto)
+  if err != nul {
+    return nil, err 
+  }
+  return &bto, nil
 }
