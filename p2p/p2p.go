7d840c71a18ace44ba84cfd9aff8229f5fcee6ef
Advaith Doosa <advaith@doosa.org>
2020-12-16 11:04:12 +0000

    p2p - checkIntegrity, startDownloadWorker, calculate piece bounds and size

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -107,3 +107,62 @@ func attemptDownloadPiece(c *client.Client, pw *pieceWork) ([]byte, error) {
 	}
 	return state.buf, nil
 }
+
+func checkIntegrity(pw *pieceWork, buf []byte) error {
+	hash := sha1.Sum(buf1)
+	if !bytes.Equal(hash[:], pw.hash[:]) {
+		return fmt.Errorf("Index %d failed integrity check", pw.index)
+	}
+	return nil
+}
+
+func (t *Torrrent) startDownloadWorker(peer peers.Peer, workQueue chan *pieceWork, results chan *pieceResult) {
+	c, err := client.New(peer, t.PeerID, t.InfoHash)
+	if err != nil {
+		log.Printf("Could not handshake with %s. Disconnecting \n", peer.IP)
+		return
+	}
+	defer c.Conn.Close()
+	log.Printf("Completed handshake with %s \n", peer.IP)
+
+	c.SendUnchoke()
+	c.SendInterested()
+
+	for pw := range workQueue {
+		if !c.Bitfield.HasPiece(pw.index) {
+			workQUeue <- pw
+			continue
+		}
+
+		buf, err := attemptDownloadPiece(c, pw)
+		if err != nil {
+			log.Println("Exiting", err)
+			workQUeue <- pw
+			return
+		}
+
+		err = checkIntegrity(pw, buf)
+		if err != nil {
+			log.Printf("Piece #%d failed integrity check\n", pw.index)
+			workQueue <- pw
+			continue
+		}
+
+		c.SendHave(pw.index)
+		results <- &pieceResult{pw.index, buf}
+	}
+}
+
+func (t *Torrent) calculateBoundsForPiece(index int) (begin int, end int) {
+	begin = index * t.PiecesLength
+	end = begin + t.PiecesLength
+	if end > t.Length {
+		end = t.Length
+	}
+	return begin,end
+}
+
+func (t *Torrent) calculatePieceSize(index int) int {
+	begin, end := t.calculateBoundsForPiece(index)
+	return end - begin
+}
